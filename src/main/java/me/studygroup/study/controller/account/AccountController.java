e5bdf53204d680366c7d524d488fa0ff13f66cb8
YoungdonJung <yeavov@gmail.com>
2020-06-27 15:57:48 +0900

    Sign-up form submit handler

--- src/main/java/me/studygroup/study/controller/account/AccountController.java
+++ src/main/java/me/studygroup/study/controller/account/AccountController.java
@@ -1,16 +1,41 @@
 package me.studygroup.study.controller.account;
 
-import org.dom4j.rule.Mode;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBInder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model){
         model.addAttribute(new SignUpForm());
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors){
+        if(errors.hasErrors()){
+            return "account/sign-up";
+        }
+
+        // TODO 회원 가입 처리
+        return "redirect:/";
+        
     }
 }
