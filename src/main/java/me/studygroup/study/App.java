abf93e4938667f0d120315f37bcad72f7ded8fa1
DDony <yellowjung@naver.com>
2020-07-09 09:32:29 +0900

    Test add and push

--- src/main/java/me/studygroup/study/App.java
+++ src/main/java/me/studygroup/study/App.java
@@ -3,6 +3,8 @@ package me.studygroup.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Start
+
 @SpringBootApplication
 public class App {
 
