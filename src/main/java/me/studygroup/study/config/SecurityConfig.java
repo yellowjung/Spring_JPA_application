3f3d2632a9f0218607bbaabff248840632c519e3
YoungdonJung <yeavov@gmail.com>
2020-08-26 16:45:41 +0900

    Finish remember Me Setting

--- src/main/java/me/studygroup/study/config/SecurityConfig.java
+++ src/main/java/me/studygroup/study/config/SecurityConfig.java
@@ -1,16 +1,29 @@
 package me.studygroup.study.config;
 
+import lombok.RequiredArgsConstructor;
+import me.studygroup.study.account.AccountService;
+import org.hibernate.engine.jdbc.connections.internal.DatasourceConnectionProviderImpl;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
+import java.nio.channels.AcceptPendingException;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AccountService accountService;
+    private final DataSource datasource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
@@ -25,6 +38,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.logout()
                 .logoutSuccessUrl("/");
+
+        http.rememberMe()
+                .userDetailsService(accountService)
+                .tokenRepository(tokenRepository());
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository(){
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(datasource);
+
+        return jdbcTokenRepository;
     }
 
     @Override
