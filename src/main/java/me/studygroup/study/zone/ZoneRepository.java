ef0548902055985088b48944a157dbf99e469ffb
YoungdonJung <yeavov@gmail.com>
2020-09-11 13:38:50 +0900

    Add zone domain

---
+++ src/main/java/me/studygroup/study/zone/ZoneRepository.java
@@ -0,0 +1,10 @@
+package me.studygroup.study.zone;
+
+
+import me.studygroup.study.donmain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+
+
+}
